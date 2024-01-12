87734fbcf4e9672690b2ac64c85068c4280836ff
Shaykhudinov Tima <shaykhudinov.tima@mail.ru>
2024-01-12 17:31:56 +0600

    first commit

---
+++ 1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homm26
+{
+    using System;
+
+    class Lamp
+    {
+        public bool TurnedOn { get; set; }
+
+        public string GetLampStatus()
+        {
+            return TurnedOn ? "Lamp|Лампа включена" : "Lamp|Лампа выключина";
+        }
+    }
+
+    class Cat
+    {
+        public string WhatIdDoingNow()
+        {
+            return "Cat|Кошка играет";
+        }
+    }
+
+    class Man
+    {
+        public string WhatHeIsDoingNow()
+        {
+            return "Man|Человек спит";
+        }
+    }
+
+    class Program
+    {
+        delegate string GetStatusDelegate();
+
+
+        static void Main()
+        {
+            Lamp lamp = new Lamp { TurnedOn = true };
+            Cat cat = new Cat();
+            Man man = new Man();
+
+            ReadState(lamp.GetLampStatus);
+            ReadState(cat.WhatIdDoingNow);
+            ReadState(man.WhatHeIsDoingNow);
+            Console.ReadLine();
+        }
+
+        static void ReadState(GetStatusDelegate del)
+        {
+            var status = del.GetInvocationList();
+            foreach (GetStatusDelegate getStatus in status)
+            {
+                Console.WriteLine(getStatus());
+            }
+        }
+    }
+}
---
+++ 2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homm26._2
+{
+    using System;
+
+    class MyMessage
+    {
+        public string MessageType { get; }
+        public string Message { get; }
+
+        public MyMessage(string type, string message)
+        {
+            MessageType = type;
+            Message = message;
+        }
+    }
+
+    class Phone
+    {
+
+        MessageManager messageManager = new MessageManager();
+        private void ReceiveSms(MyMessage msg)
+        {
+            if (msg.MessageType == "sms")
+            {
+                Console.WriteLine($"Получил смс {msg.Message}");
+            }
+        }
+        public void Sms()
+        {
+            messageManager.NewMessage += ReceiveSms;
+        }
+    }
+
+    class Mail
+    {
+        MessageManager messageManager = new MessageManager();
+        private void ReceiveMail(MyMessage msg)
+        {
+            if (msg.MessageType == "email")
+            {
+                Console.WriteLine($"Получил письмо {msg.Message}");
+            }
+        }
+        public void Email()
+        {
+            messageManager.NewMessage += ReceiveMail;
+        }
+    }
+
+    class MessageManager
+    {
+        public event Action<MyMessage> NewMessage;
+
+        public void SendMessage(MyMessage message)
+        {
+            NewMessage?.Invoke(message);
+        }
+    }
+
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            MessageManager messageManager = new MessageManager();
+
+            Phone phone = new Phone();
+            Mail mail = new Mail();
+
+            phone.Sms();
+            mail.Email();
+
+            Console.WriteLine("Отправка сообщения");
+            Console.Write("Введите тип сообщения: ");
+            string messageType = Console.ReadLine();
+            Console.Write("Введите сообщение: ");
+            string messageText = Console.ReadLine();
+
+            MyMessage message = new MyMessage(messageType, messageText);
+            messageManager.SendMessage(message);
+
+            Console.ReadLine();
+        }
+    }
+}
